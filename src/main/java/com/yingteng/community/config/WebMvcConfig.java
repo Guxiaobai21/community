1aa1471e79e2ed0e74f40c128af862d1882e6df9
GoodFood <827413082@qq.com>
2020-09-24 21:55:50 +0800

    添加拦截器

---
+++ src/main/java/com/yingteng/community/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package com.yingteng.community.config;
+
+import com.yingteng.community.controller.interceptor.AlphaInterceptor;
+import com.yingteng.community.controller.interceptor.LoginTicketInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//配置拦截器，需要实现 WebMvcConfigurer 接口
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AlphaInterceptor alphaInterceptor;
+
+    @Autowired
+    private LoginTicketInterceptor loginTicketInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //添加一个拦截器
+        registry.addInterceptor(alphaInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg")//哪些路径不拦截（一般排除静态资源）
+                .addPathPatterns("/register", "/login"); //指定拦截的请求路径
+
+        registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+    }
+}
---
+++ src/main/java/com/yingteng/community/util/HostHolder.java
@@ -0,0 +1,28 @@
+package com.yingteng.community.util;
+
+import com.yingteng.community.entity.User;
+import org.springframework.stereotype.Component;
+
+/**
+ * 持有用户信息，用于代替session对象。
+ */
+@Component
+public class HostHolder {
+
+    //实现了线程隔离，存值
+    private ThreadLocal<User> users = new ThreadLocal<>();
+
+
+    public void setUser(User user){
+        users.set(user);
+    }
+
+    public User getUser() {
+        return users.get();
+    }
+
+    public void clear(){
+        users.remove();
+    }
+
+}
