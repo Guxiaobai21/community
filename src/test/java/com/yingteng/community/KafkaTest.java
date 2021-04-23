956be8046e5e18acec3a19718aa26142b1af2d79
GoodFood <827413082@qq.com>
2021-04-23 10:45:54 +0800

    Kafka消息发布与订阅

---
+++ src/test/java/com/yingteng/community/KafkaTest.java
@@ -0,0 +1,55 @@
+package com.yingteng.community;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.record.Record;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.listener.Topic;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.function.Consumer;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class KafkaTest {
+
+    @Autowired
+    private KafkaProducer producer;
+
+    @Test
+    public void testKafka(){
+        producer.sendMessage("test", "发送");
+        producer.sendMessage("test", "kafka");
+        producer.sendMessage("test", "消息");
+        try {
+            Thread.sleep(1000 * 10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+@Component
+class KafkaProducer{
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public void sendMessage(String topic, String content){
+        kafkaTemplate.send(topic, content);
+    }
+}
+
+@Component
+class KafkaConsumer{
+
+    @KafkaListener(topics = {"test"})
+    public void handleMessage(ConsumerRecord record){
+        System.out.println(record.value());
+    }
+
+}
